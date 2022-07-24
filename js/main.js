47e498f066ebb7cd19f458fa7cb48029c52cb2e5
MohamedTarek1993 <www.mtarek789@gmail.com>
2022-07-24 11:11:49 +0200

    24/7

--- js/main.js
+++ js/main.js
@@ -54,3 +54,14 @@ $('#cmd').click(function () {
   });
   doc.save('cv.pdf');
 });
+
+//=========== Active navbar Link in multi pages =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active";
+  }
+}
+//=========== Active navbar Link in multi pages =============
